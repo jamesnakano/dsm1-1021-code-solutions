11d615c48b180de5523108ad93d7de0d0c9775cb
James Nakano <jamesmnakano@gmail.com>
2021-10-19 18:11:01 -0700

    Completed ds prep example exercise

---
+++ ds-prep-example.tar/ds-prep-example/example.py
@@ -0,0 +1,17 @@
+print("""
+  ===============
+  ds-prep-example
+  ===============
+""")
+
+print("I would like to analyze {} to learn/predict {}.\n".format(
+
+  # write what you would like to analyze in the quotes here 👇
+
+  "What size drink do adults buy for their kids",
+
+  # write what you would like to learn from that analysis here 👇
+
+  "How much extra money will adults spend on their kids"
+
+))
