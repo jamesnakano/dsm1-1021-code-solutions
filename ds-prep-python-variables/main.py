921140a74919786363fb557ab4de506b6ea2edd6
James Nakano <jamesmnakano@gmail.com>
2021-10-21 18:00:44 -0700

    Updated ds-prep-python-variables excercise.

--- ds-prep-python-variables/main.py
+++ ds-prep-python-variables/main.py
@@ -12,14 +12,8 @@ print(height_in_meters)
 
 eats_plants = True
 eats_animals = False
-if eats_plants == True or eats_animals == True:
-  is_animal = True
-else:
-  is_animal = False
-if eats_plants == True and eats_animals == True:
-  is_omnivore = True
-else:
-  is_omnivore = False
+is_animal = (eats_plants == True or eats_animals == True)
+is_omnivore = (eats_plants == True and eats_animals == True)
 is_plant = not is_animal
 print(is_animal, is_omnivore, is_plant)
 
@@ -28,7 +22,7 @@ short_threshold_in_meters = 1.576
 tall_threshold_in_meters = 1.860
 is_mean_height = height_in_meters == mean_height_in_meters
 is_short = height_in_meters < short_threshold_in_meters
-is_tall = height_in_meters > tall_threshold_in_meters
+is_tall = height_in_meters >= tall_threshold_in_meters
 is_normal_height = height_in_meters >= short_threshold_in_meters and height_in_meters < tall_threshold_in_meters
 print(is_mean_height, is_tall, is_short, is_normal_height)
 
