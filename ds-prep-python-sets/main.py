25094b320a26bef4563e8dd7b3ccf4c01431a7af
James Nakano <jamesmnakano@gmail.com>
2021-12-14 19:18:40 -0800

    Completed python-sets exercise

---
+++ ds-prep-python-sets/main.py
@@ -0,0 +1,16 @@
+trail_mix = {'m&ms', 'peanuts', 'raisins'}
+print(trail_mix)
+print('cashews' in trail_mix)
+print('peanuts' in trail_mix)
+trail_mix.add('pretzels')
+trail_mix.update(['peanuts', 'banana chips', 'bits of jerky'])
+print(trail_mix)
+trail_mix.remove('m&ms')
+trail_mix.discard('raisins')
+trail_mix.discard('rat poison')
+print(trail_mix)
+nuts = {'peanuts', 'cashews', 'almonds', 'walmuts', 'peacans'}
+print(nuts.union(trail_mix))
+print(nuts.intersection(trail_mix))
+print(trail_mix.difference(nuts))
+print(nuts.difference(trail_mix))
